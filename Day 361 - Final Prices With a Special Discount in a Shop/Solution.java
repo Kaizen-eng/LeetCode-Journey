2ce078bec76f55d616086e2b820d23cdf0b90ff1
PRASAD <prasadid1125@gmail.com>
2026-08-02 09:38:50 +0530

    Implement finalPrices method to calculate discounts

---
+++ Day 361 - Final Prices With a Special Discount in a Shop/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] finalPrices(int[] prices) {
+
+        int[] ans = new int[prices.length];
+
+        for (int i = 0; i < prices.length; i++) {
+
+            for (int j = i; j < prices.length; j++) {
+
+                if (j > i && prices[j] <= prices[i]) {
+                    ans[i] = prices[i] - prices[j];
+                    break;
+                } else {
+                    ans[i] = prices[i];
+                }
+            }
+        }
+
+        return ans;
+    }
+}
