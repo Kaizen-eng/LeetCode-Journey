0612ef18c897689ac06523f8ec946981dfc93391
PRASAD <prasadid1125@gmail.com>
2026-08-11 23:26:02 +0530

    Create Solution.java

---
+++ Day 369 - Reverse Prefix of Word/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+
+        int index = word.indexOf(ch);
+
+        StringBuilder sb = new StringBuilder(word.substring(0, index + 1));
+
+        sb.reverse();
+
+        return sb.toString() + word.substring(index + 1);
+    }
+}
