f879b7c9fd4cd30f73ad6e52ddbc9d6b24076148
PRASAD <prasadid1125@gmail.com>
2026-09-10 12:32:37 +0530

    Implement solution to count students unable to eat

---
+++ Day 400 - Number of students unable to eat lunch/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+
+        int[] count = new int[2];
+
+        // Count student preferences
+        for (int student : students) {
+            count[student]++;
+        }
+
+        // Process sandwiches from top to bottom
+        for (int sandwich : sandwiches) {
+
+            if (count[sandwich] == 0) {
+                break;
+            }
+
+            count[sandwich]--;
+        }
+
+        return count[0] + count[1];
+    }
+}
